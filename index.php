f483eab77eb9cbdbaf78d35cd3b41fa1214b5bfd
Anggarda Tiratana <anggardatiratana@gmail.com>
2023-06-21 14:26:38 +0700

    Add Live Chat Support

--- index.php
+++ index.php
@@ -206,6 +206,21 @@ else: ?><!DOCTYPE html>
 
         });
         </script>
+        
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64929ffdcc26a871b023ca6d/1h3ec1udc';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
     </body>
 </html>
 
